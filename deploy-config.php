af4a5471470a92766ed0be1c2f5f8816b5bf4008
alexzer0 <alexanderc4st@gmail.com>
2017-03-05 10:01:05 -0500

    deploy cambios

--- deploy-config.php
+++ deploy-config.php
@@ -4,4 +4,4 @@ define('REMOTE_REPOSITORY', 'git@github.com:alexzer0/imad-web.git');
 define('DIR','/home/imad/public_html/'); // It's important that the path ends with '/'
 define('BRANCH','refs/heads/master'); // (Gitlab branch Route)
 define('LOGFILE', "log.txt"); // Log File Name
-define('GIT', '/usr/bin/git'); // Uni
+define('GIT', '/usr/local/git/libexec/git-core/git'); // Uni
